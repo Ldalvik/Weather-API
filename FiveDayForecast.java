1090c3630fdde001bd4c7c7ad03df153c8018e26
Julian Osborne <subaruforge@gmail.com>
2018-07-09 14:07:52 -0400

    Create FiveDayForecast.java

---
+++ FiveDayForecast.java
@@ -0,0 +1,27 @@
+package yo;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import fiveday.Forecast;
+
+import java.io.IOException;
+
+public class FiveDayForecast {
+    private String URL_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast?";
+    public JSONArray forecasts;
+
+    public FiveDayForecast(String zipcode, String country, String API_KEY) {
+        try {
+            String json = Utils.getRequest(URL_ENDPOINT + "zip=" + zipcode + "," + country + "&APPID=" + API_KEY);
+            JSONTokener token = new JSONTokener(json);
+            this.forecasts = new JSONObject(token).getJSONArray("list");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public Forecast getForecast(int place) {
+            return new Forecast(forecasts.getJSONObject(place));
+        }
+}
