66aecd18748100cab6d8763aa85e676b1556ddd0
Julian Osborne <subaruforge@gmail.com>
2018-07-09 14:05:55 -0400

    Create Weather.java

---
+++ current/Weather.java
@@ -0,0 +1,28 @@
+package current;
+
+import org.json.JSONObject;
+
+public class Weather
+{
+    private JSONObject obj;
+
+    public Weather(JSONObject obj){
+    this.obj = obj;
+    }
+
+    public int getId () {
+        return obj.getInt("id");
+    }
+
+    public String getIcon () {
+        return obj.getString("icon");
+    }
+
+    public String getDescription () {
+        return obj.getString("description");
+    }
+
+    public String getMain () {
+        return obj.getString("main");
+    }
+}
