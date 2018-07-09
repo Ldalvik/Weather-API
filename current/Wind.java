29c4378f37a385c3dbb67bc7b698e7467db99951
Julian Osborne <subaruforge@gmail.com>
2018-07-09 14:06:26 -0400

    Create Wind.java

---
+++ current/Wind.java
@@ -0,0 +1,19 @@
+package current;
+
+import org.json.JSONObject;
+
+public class Wind {
+    private JSONObject obj;
+
+    public Wind(JSONObject obj) {
+        this.obj = obj;
+    }
+
+    public double getSpeed() {
+        return (double) obj.get("speed");
+    }
+    public int getDegrees() {
+        return obj.getInt("deg");
+    }
+
+}
